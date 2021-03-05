4fb3cd747410b0ec403eb5fe7c51134c2c65b37d
Pieter <pieter.miedema@hva.nl>
2021-03-05 20:04:27 +0100

    homepage

--- app/src/main/java/com/example/sambandh_20/MainActivity.kt
+++ app/src/main/java/com/example/sambandh_20/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.sambandh_20
 
 import android.content.Intent
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
+import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.getBitmap
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.example.sambandh_20.model.User
 import com.example.sambandh_20.ui.login.LoginActivity
 import com.example.sambandh_20.ui.register.RegisterActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,17 +32,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.title ="Sambandh"
 
-        if (FirebaseAuth.getInstance().currentUser == null) {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
         verifyUserLoggedIn()
+        setUserImage()
+    }
+
+    private fun setUserImage() {
+        val uid = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        var profileImageUrl: String? = ""
+
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                profileImageUrl = snapshot.child("profileImageUrL").getValue(String::class.java)
+                Log.d("test", profileImageUrl.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                sendToast("Something went wrong")
+            }
+        })
+
+        if (profileImageUrl!!.isNotEmpty()){
+            activity_home_btn_profile.setImageURI(Uri.parse(profileImageUrl.toString()))
+
+        }
+        else {
+            activity_home_btn_profile.setImageResource(R.drawable.henk)
+        }
     }
 
     fun verifyUserLoggedIn(){
         val uid = FirebaseAuth.getInstance().uid
         if (uid == null){
-            val intent = Intent(this, RegisterActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
@@ -50,6 +89,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.nav_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    //sends short toast message to the user
+    fun sendToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 
--- app/src/main/java/com/example/sambandh_20/model/User.kt
+++ app/src/main/java/com/example/sambandh_20/model/User.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class User(val uid: String, val profileImageUrL: String, val displayName: String, val firstName: String,
+data class User(val uid: String, val profileImageUrL: String, val displayName: String, val firstName: String,
            val lastName: String, val dateOfBirth: String): Parcelable {
     constructor() : this("","","", "", "", "")
 }
